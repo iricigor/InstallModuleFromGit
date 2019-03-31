d48b500491e8fbfcce3f1626ac8eec06b2b74790
Igor Iric <iricigor@outlook.com>
2019-03-31 19:39:17 +0200

    got the error

--- Public/Install-GitModule.ps1
+++ Public/Install-GitModule.ps1
@@ -43,6 +43,8 @@ function Install-GitModule {
 
             $ModuleInfo = Get-GitModule -ProjectUri $P1 -KeepTempCopy
             if (!$ModuleInfo -or ($ModuleInfo.Count -gt 1)) {
+                Write-Verbose "Count: " + ($ModuleInfo.Count)
+                Write-Verbose "Names: " + ($ModuleInfo.Name -join ',')
                 Write-Error "$FunctionName cannot identify unique module"
                 continue
             } # we have the error in get-gitmodule
