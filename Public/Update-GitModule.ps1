d8e1edf31b40a0282889873c442c6df9fc74b913
Igor Iric <iricigor@outlook.com>
2019-04-18 23:53:22 +0200

    unified code

--- Public/Update-GitModule.ps1
+++ Public/Update-GitModule.ps1
@@ -39,11 +39,25 @@ function Update-GitModule {
 
             Write-Verbose -Message "$(Get-Date -f T)   processing $P1"
 
-            $ModuleInfo = Get-GitModule -ProjectUri $P1 -KeepTempCopy
-            if (!$ModuleInfo -or ($ModuleInfo.Count -gt 1)) {continue} # we have the error in get-gitmodule
+            $RemoteModuleInfo = Get-GitModule -ProjectUri $P1 -KeepTempCopy
+            if (!$RemoteModuleInfo -or ($RemoteModuleInfo.Count -gt 1)) {continue} # we have the error in get-gitmodule
+            $ModuleName = $RemoteModuleInfo.Name
 
-            # TODO: Check version, and if higher install it
-            # TODO: Unify code for two scripts: this and Install-GitModule
+            # TODO: continue only after cleanup!
+
+            # Check version, and if higher install it
+            $LocalModuleInfo = Get-Module -Name $ModuleName -ListAvailable | Sort-Object Version -Descending | Select -First 1
+            if (!$LocalModuleInfo) {
+                Write-Error "$FunctionName cannot find local module '$ModuleName'"
+                continue
+            }
+            if ($LocalModuleInfo.Version -ge $RemoteModuleInfo.Version) {
+                Write-Verbose "$(Get-Date -f T)   not updating module '$ModuleName', local version $($LocalModuleInfo.Version), remote version $($RemoteModuleInfo.Version)"
+            } else {
+                Install-ModuleInfo -ModuleInfo $ModuleInfo -DestinationPath $DestinationPath -Force:$Force
+            }
+
+            
         }
     }
 
