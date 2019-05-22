4b45e77988acd405c2d5421cbce24753f6bd6f6f
Igor Iric <iiric@microsfot.com>
2019-05-22 13:07:49 +0200

    adding blank tests file

---
+++ Tests/functions/Update-GitModule.Tests.ps1
@@ -0,0 +1,25 @@
+#
+# This is a PowerShell Unit Test file.
+# You need a unit test framework such as Pester to run PowerShell Unit tests.
+# You can download Pester from http://go.microsoft.com/fwlink/?LinkID=534084
+#
+
+$CommandName = 'Update-GitModule'
+
+Describe "$CommandName basic testing" -Tag 'Functionality' {
+
+
+    # TODO: Add real tests here
+
+    #
+    # Fake test
+    #
+
+    Describe "$CommandName Fake-Test" -Tag 'Other' {
+        It "Should be fixed by developer" {
+            $false | Should -Be $true
+        }
+    }
+
+
+}
