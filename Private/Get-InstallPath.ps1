6718ae7099710bbbf697a489e86d15b82168807b
Igor Iric <iricigor@outlook.com>
2019-06-14 01:40:41 +0200

    update logic

--- Private/Get-InstallPath.ps1
+++ Private/Get-InstallPath.ps1
@@ -1,7 +1,8 @@
 function Get-InstallPath {
 
     # returns OS specific path for module installation, it support only -Scope CurrentUser
-    if ($IsLinux -or $IsOSX) {
+    
+    $defaultPath = if ($IsLinux -or $IsOSX) {
         #"$HOME/.local/share/powershell/Modules"
         # https://github.com/PowerShell/PowerShellGet/blob/d4dfebbbec4dfbe73392719a8a331541ed75d508/src/PowerShellGet/private/modulefile/PartOne.ps1#L71
         Join-Path (Split-Path -Path ([System.Management.Automation.Platform]::SelectProductNameForDirectory('USER_MODULES')) -Parent) 'Modules'
@@ -18,4 +19,22 @@ function Get-InstallPath {
         }
     }
 
+    $ModulePaths = $Env:PSModulePath -split (';:'[[int]($IsLinux -or $IsMacOS)])
+    if ($defaultPath -in $ModulePaths) {
+        $defaultPath
+    } else {
+        # default path is not in findable by get-module, try to avoid it
+        $writablePath = ''
+        foreach ($P1 in $ModulePaths) {
+            if (([string]::IsNullOrEmpty($writablePath)) -and (Test-PathWritable $P1)) {
+                $writablePath = $P1
+            }
+        }
+        if ([string]::IsNullOrEmpty($writablePath)) {
+            # we found no writable paths, return default one
+            $defaultPath
+        } else {
+            $writablePath
+        }
+    }
 }
