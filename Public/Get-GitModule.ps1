3da0a9aeefcda5f62334047c10f5ec688f2acfcd
Igor <iricigor@outlook.com>
2019-06-17 22:25:29 +0200

    added test (#43)
    * added test
    
    * separe to finction and add to update
    
    * documentation

--- Public/Get-GitModule.ps1
+++ Public/Get-GitModule.ps1
@@ -33,27 +33,7 @@ function Get-GitModule {
             $tmpRoot = [System.IO.Path]::GetTempPath()
         }     
         
-        if ($Name) {
-            Write-Verbose -Message "$(Get-Date -f T)   searching module URIs from their names"
-            $ProjectUri = foreach ($N1 in $Name) {
-                $Module = Get-InstalledModule $N1 -ea 0
-                if (!$Module) {$Module = Get-Module $N1 -ListAvailable -ea 0}
-
-                if (!$Module) {
-                    Write-Error "$FunctionName found no module $N1"
-                    continue
-                } 
-
-                if (!($Module | ? ProjectUri)) {
-                    Write-Warning "$FunctionName found module $N1, but it has no ProjectUri information"
-                    continue
-                }
-
-                # return information to $ProjectUri variable
-                $Module | Sort-Object Version | Select-Object -Last 1 -ExpandProperty ProjectUri
-
-            }
-        }
+        if ($Name) {$ProjectUri = ConvertTo-Uri -Name $Name}
 
     }
 
--- Tests/functions/Update-GitModule.Tests.ps1
+++ Tests/functions/Update-GitModule.Tests.ps1
@@ -36,6 +36,36 @@ Describe "$CommandName basic testing" -Tag 'Functionality' {
 
 }
 
+Describe "$CommandName byName testing" -Tag 'Functionality' {
+
+
+    $moduleName = 'FIFA2018'
+    $moduleURL = 'https://github.com/iricigor/' + $moduleName
+
+    $ExistingModule = Get-InstalledModule $moduleName -ea 0
+    It 'Should uninstall again module if installed' -Skip:($ExistingModule -eq $null) {
+        {Uninstall-Module $moduleName} | Should -Not -Throw
+    }
+
+    # its not so easy to remove PowerShell module
+    $ExistingModule = Get-Module $moduleName -ListAvailable
+    It 'Should delete again module if still found' -Skip:($ExistingModule -eq $null) {
+        {Split-Path ($ExistingModule.Path) -Parent | Remove-Item -Force -Recurse} | Should -Not -Throw
+    }
+
+    It 'Should install again module from PSGallery' {
+        $OldProgressPreference = $ProgressPreference
+        $ProgressPreference = 'SilentlyContinue'
+        {Install-Module $moduleName -Repository PSGallery -Scope CurrentUser -Force} | Should -Not -Throw
+        $ProgressPreference = $OldProgressPreference
+    }
+
+    It 'Should update module byName to newer version' {
+        Update-GitModule -Name $moduleName -Force | Should -Not -Be $null
+    }
+
+}
+
 
 Describe "$CommandName error handling" -Tag 'Functionality' {
 
